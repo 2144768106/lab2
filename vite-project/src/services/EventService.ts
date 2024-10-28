5a9d0252fb6f5502b360994b29bb6ce078366602
2144768106 <2144768106qq.com>
2024-10-28 21:16:17 +0800

    2

--- vite-project/src/services/EventService.ts
+++ vite-project/src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: '[your mock server url]',
+  baseURL: 'https://my-json-server.typicode.com/2144768106/lab2/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
